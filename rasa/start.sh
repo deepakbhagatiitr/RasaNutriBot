51d529193d35571c6d95d4e4322ed90d8d058854
Deepak Bhagat <deepak988088@gmail.com>
2025-04-05 17:36:10 +0530

    update sh

--- rasa/start.sh
+++ rasa/start.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+
+# PORT=${PORT:-10000}  # fallback to 10000 if PORT is not set
 echo "Starting Rasa on Render-assigned port: $PORT"
-rasa run --enable-api --cors "*" --debug --port ${PORT}
+
+rasa run --enable-api --cors "*" --debug --port $PORT --host 0.0.0.0
