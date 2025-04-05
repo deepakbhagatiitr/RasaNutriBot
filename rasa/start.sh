e888323504b911081faa803459cbf68d33efb20c
Deepak Bhagat <deepak988088@gmail.com>
2025-04-05 18:04:03 +0530

    Make start.sh executable

--- rasa/start.sh
+++ rasa/start.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-# PORT=${PORT:-10000}
+PORT=${PORT:-10000}
 echo "Starting Rasa on Render-assigned port: $PORT"
 
-rasa run --enable-api --cors "*" --debug --port $PORT --host=0.0.0.0
+rasa run \
+  --enable-api \
+  --cors "*" \
+  --debug \
+  --port $PORT \
+  --host 0.0.0.0
